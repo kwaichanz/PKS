d46afdea5b01c6b298770d419b4c9565dff90aab
Kwaichanz <alphezelphaz@gmail.com>
2023-06-22 16:31:32 +0700

    added server

---
+++ TGT-APP/src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between ">
+      <button className="btn" type="button">
+        {" "}
+        Click me!
+      </button>
+    </div>
+  );
+};
---
+++ TGT-APP/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui:{
+    themes: ["light","dark","retro"],
+  },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: [ {brand: {
+ "primary": "#8cea94",
+ "secondary": "#af0ef4",
+ "accent": "#360199",
+ "neutral": "#262f3b",
+ "base-100": "#e9e9ed",
+ "info": "#98d2e7",
+ "success": "#2ebdaf",
+ "warning": "#f2c150",
+ "error": "#df3a5d",
+  }},{
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },],
+  plugins: [require("daisyui")],
+};
