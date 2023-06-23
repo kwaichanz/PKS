326859edfd147d7772a297a1ecc41fba24cb7e33
Kwaichanz <alphezelphaz@gmail.com>
2023-06-23 15:22:43 +0700

    update basic carousel for hero

--- TGT-APP/src/components/Hero/Hero.tsx
+++ TGT-APP/src/components/Hero/Hero.tsx
@@ -1,10 +1,31 @@
+"use client";
+
 import React from "react";
 import { HeroCorousel } from "../HeroCarousel/HeroCarousel";
+import { items } from "../../../mocks/HeroCarouselItems.json";
+import Image from "next/image";
 
 export const Hero = () => {
+  const { responsive } = items;
+
   return (
-    <div className=" w-full bg-orange-100 h-[54rem]">
-      <HeroCorousel />
+    <div className=" w-full mx-auto my-2 h-[56rem] bg-gray-200">
+      <HeroCorousel loop>
+        {responsive.map((item) => {
+          return (
+            <div className="relative h-screen flex-[0_0_100%]" key={item.id}>
+              <Image
+                src={item.imageUrl}
+                alt={item.title}
+                className=" object-cover"
+                loading="lazy"
+                fill
+              />
+              d
+            </div>
+          );
+        })}
+      </HeroCorousel>
     </div>
   );
 };
--- TGT-APP/src/components/HeroCarousel/HeroCarousel.tsx
+++ TGT-APP/src/components/HeroCarousel/HeroCarousel.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { items } from "../../../mocks/HeroCarouselItems.json";
+import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
 
-export const HeroCorousel = () => {
-  const { responsive } = items;
+type Props = PropsWithChildren & EmblaOptionsType;
+
+export const HeroCorousel = ({ children, ...options }: Props) => {
+  // Use the `useEmblaCarousel` hook to create an embla API instance.
+  // Returnns a emblaRef and attach the ref to a containers element.
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
   return (
-    <div className=""></div>
+    <div className=" overflow-hidden" ref={emblaRef}>
+      <div className="flex">{children}</div>
+    </div>
   );
 };
