cdb3eaed47beb7d5fa7d7a3a1e5645cbad610117
kwaichanz <alphezelphaz@gmail.com>
2023-07-01 23:33:04 +0700

    remove unncessary files

--- fib.js
+++
@@ -1,9 +0,0 @@
-const { workerData, parentPort } = require("worker_threads");
-
-function fib(n) {
-  return n < 1 ? 0 : n < 2 ? 1 : fib(n - 1) + fib(n - 2);
-}
-
-const result = fib(workerData.iterations);
-
-parentPort.postMessage(result);
